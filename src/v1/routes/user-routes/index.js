ac80dca6175117741ce11e06f337d25031ff39de
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-06-18 11:34:54 +0300

    Update user routes by adding verify token middleware

--- src/v1/routes/user-routes/index.js
+++ src/v1/routes/user-routes/index.js
@@ -11,6 +11,7 @@ const {
 } = require("../../../controllers/user-controller");
 
 // Import middleware
+const { verifyToken } = require("../../../middlewares/auth/jwt");
 const { validateSchema } = require("../../../middlewares/validate-schema");
 
 // Import schema
@@ -22,15 +23,15 @@ const { updateUserSchema } = require("../../../validation/user-schema/update");
 router.post("/", validateSchema(userSchema), createNewUser);
 
 // 2. Get all users
-router.get("/", getAllUsers);
+router.get("/", verifyToken, getAllUsers);
 
 // 3. Get one user by id
-router.get("/:userId", getOneUser);
+router.get("/:userId", verifyToken, getOneUser);
 
 // 4. Update one user by id
-router.patch("/:userId", validateSchema(updateUserSchema), updateOneUser);
+router.patch("/:userId", verifyToken, validateSchema(updateUserSchema), updateOneUser);
 
 // 5. Delete one user by id
-router.delete("/:userId", deleteOneUser);
+router.delete("/:userId", verifyToken, deleteOneUser);
 
 module.exports = router;
