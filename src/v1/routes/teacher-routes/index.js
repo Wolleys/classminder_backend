1898ac163cadec5b3e320e657e18d9f00b7aed97
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-06-18 11:27:38 +0300

    Update teacher routes by adding verify token middleware

--- src/v1/routes/teacher-routes/index.js
+++ src/v1/routes/teacher-routes/index.js
@@ -13,32 +13,40 @@ const {
 } = require("../../../controllers/teacher-controller");
 
 // Import middleware
+const { verifyToken } = require("../../../middlewares/auth/jwt");
 const { validateSchema } = require("../../../middlewares/validate-schema");
 
 // Import schema
 const { teacherSchema } = require("../../../validation/teacher-schema");
-const { updateTeacherSchema } = require("../../../validation/teacher-schema/update");
+const {
+    updateTeacherSchema,
+} = require("../../../validation/teacher-schema/update");
 
 // Teacher routes
 // 1. Create a new teacher
-router.post("/", validateSchema(teacherSchema), createNewTeacher);
+router.post("/", verifyToken, validateSchema(teacherSchema), createNewTeacher);
 
 // 2. Get all teachers
-router.get("/", getAllTeachers);
+router.get("/", verifyToken, getAllTeachers);
 
 // 3. Get one teacher by id
-router.get("/:teacherId", getOneTeacher);
+router.get("/:teacherId", verifyToken, getOneTeacher);
 
 // 4. Update one teacher by id
-router.patch("/:teacherId", validateSchema(updateTeacherSchema), updateOneTeacher);
+router.patch(
+    "/:teacherId",
+    verifyToken,
+    validateSchema(updateTeacherSchema),
+    updateOneTeacher
+);
 
 // 5. Delete one teacher by id
-router.delete("/:teacherId", deleteOneTeacher);
+router.delete("/:teacherId", verifyToken, deleteOneTeacher);
 
 // 6. Delete one courseId from teacher by id
-router.delete("/:teacherId/courses/:courseId", deleteCourseId);
+router.delete("/:teacherId/courses/:courseId", verifyToken, deleteCourseId);
 
 // 7. Delete one classId from teacher by id
-router.delete("/:teacherId/classes/:classId", deleteClassId);
+router.delete("/:teacherId/classes/:classId", verifyToken, deleteClassId);
 
 module.exports = router;
