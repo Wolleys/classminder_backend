3bd74890342a756a7d67c58bb8cdb4de42d9d260
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-06-22 15:11:03 +0300

    Update logout function to handle the corrent user logout

--- src/data-access/auth/logout.js
+++ src/data-access/auth/logout.js
@@ -1,12 +1,16 @@
 const { logoutOneEntity } = require("../modules");
 
 const logout = async (model, cookies) => {
-    // Check cookie in user table
-    const logoutUserParams = {
+    const common = {
         cookies,
-        model: model.User,
         cond: { refresh_token: cookies.jwt },
         attributes: ["id", "refresh_token"],
+    };
+
+    // Check cookie in user table
+    const logoutUserParams = {
+        model: model.User,
+        ...common,
     };
     const adminUser = await logoutOneEntity(logoutUserParams);
 
@@ -16,6 +20,36 @@ const logout = async (model, cookies) => {
             { where: { id: adminUser.id } }
         );
         return adminUser;
+    }
+
+    // Check cookie in teacher table
+    const logoutTeacherParams = {
+        model: model.Teacher,
+        ...common,
+    };
+    const teacherUser = await logoutOneEntity(logoutTeacherParams);
+
+    if (teacherUser) {
+        await teacherUser.update(
+            { refresh_token: null },
+            { where: { id: teacherUser.id } }
+        );
+        return teacherUser;
+    }
+
+    // Check cookie in student table
+    const logoutStudentParams = {
+        model: model.Student,
+        ...common,
+    };
+    const studentUser = await logoutOneEntity(logoutStudentParams);
+
+    if (studentUser) {
+        await studentUser.update(
+            { refresh_token: null },
+            { where: { id: studentUser.id } }
+        );
+        return studentUser;
     }
 };
 
